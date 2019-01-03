1dcf745fa3f8ccb15021dbcefffdc1df468f25f1
nhatvo18 <nhatvo18@gmail.com>
2019-01-03 17:32:45 -0500

    Added some CSS styling

--- health/urls.py
+++ health/urls.py
@@ -6,5 +6,4 @@ app_name = 'health'
 urlpatterns = [
     path('', views.home, name='home'),
     path('<str:symptom>/', views.diagnoses, name='diagnoses'),
-    # path('<int:diagnosis_id>/treatment/', views.treatment, name='treatment')
 ]
